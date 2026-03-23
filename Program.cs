ba17a4835d771a6da1db898b65a7afd4ce1b893d
petar1432 <petarcondickadmenovic@gmail.com>
2026-03-23 20:19:02 +0100

    Update Program.cs
    im not robot!

--- Program.cs
+++ Program.cs
@@ -13,3 +13,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Im not robot!");
+Console.WriteLine("Hello, World!");
